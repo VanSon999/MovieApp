428b243acd83de8e46ba7c7d21e4d437f6ffd167
VanSon999 <nguyenvanson95999@gmail.com>
2021-02-19 22:21:32 +0700

    setup indicator and timeming

--- app/src/main/java/vanson/dev/movieapp/HomeActivity.kt
+++ app/src/main/java/vanson/dev/movieapp/HomeActivity.kt
@@ -1,8 +1,13 @@
 package vanson.dev.movieapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_home.*
+import java.sql.Time
+import java.util.*
+import kotlin.collections.ArrayList
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var stSlides: ArrayList<Slide>
@@ -20,5 +25,25 @@ class HomeActivity : AppCompatActivity() {
 
         val mAdapter = SliderPagerAdapter(this, stSlides)
         slide_pager.adapter = mAdapter
+
+        TabLayoutMediator(indicator, slide_pager) {tab, position ->
+            tab.select()
+        }.attach()
+
+        //setup timer
+        val timer = Timer()
+        timer.scheduleAtFixedRate(SliderTimer(this, stSlides),4000, 6000)
+    }
+
+    class SliderTimer(val activity: Activity, val slides: List<Slide>) : TimerTask(){
+        override fun run() {
+            activity.runOnUiThread{
+                if(activity.slide_pager.currentItem < slides.lastIndex){
+                    activity.slide_pager.currentItem = activity.slide_pager.currentItem + 1
+                }else{
+                    activity.slide_pager.currentItem = 0
+                }
+            }
+        }
     }
 }

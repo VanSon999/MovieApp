a9b6cc96aa9c819e7c28ee336e99f4a768ddc85d
VanSon999 <nguyenvanson95999@gmail.com>
2021-02-20 16:45:34 +0700

    use exoplayer for MoviePlayerActivity

---
+++ app/src/main/java/vanson/dev/movieapp/MoviePlayerActivity.kt
@@ -0,0 +1,47 @@
+package vanson.dev.movieapp
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_movie_player.*
+
+@Suppress("DEPRECATION")
+class MoviePlayerActivity : AppCompatActivity() {
+    private lateinit var mSimpleExoPlayer: SimpleExoPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Set fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_movie_player)
+        supportActionBar?.hide()
+        //Setting ex_player
+        mSimpleExoPlayer = SimpleExoPlayer.Builder(this).build()
+        movie_exo_player.player = mSimpleExoPlayer
+        val dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "appname"))
+        val videoSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
+            MediaItem.fromUri(VIDEO_TEST_URL))
+        mSimpleExoPlayer.setMediaSource(videoSource)
+        mSimpleExoPlayer.prepare()
+        mSimpleExoPlayer.playWhenReady = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSimpleExoPlayer.release()
+    }
+
+    companion object{
+        const val VIDEO_TEST_URL = "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"
+    }
+}

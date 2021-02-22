2a91b19c2fdef025b21958801fc4dffac19fd1bf
VanSon999 <nguyenvanson95999@gmail.com>
2021-02-22 18:34:17 +0700

    change Cast Adapter use DiffUtil to manage change data

--- app/src/main/java/vanson/dev/movieapp/adapter/CastAdapter.kt
+++ app/src/main/java/vanson/dev/movieapp/adapter/CastAdapter.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_cast.view.*
 import vanson.dev.movieapp.models.Cast
 import vanson.dev.movieapp.R
+import vanson.dev.movieapp.utils.DiffCallBack
+import vanson.dev.movieapp.utils.loadImage
 
-class CastAdapter(private val mContext: Context, private val mData: List<Cast>) : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
+class CastAdapter() : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
+    private var mData: List<Cast> = listOf()
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,10 +24,16 @@ class CastAdapter(private val mContext: Context, private val mData: List<Cast>) 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder.itemView){
-            Glide.with(mContext).load(mData[position].getImgLink()).into(this.img_cast)
-            name_cast.text = mData[position].getName()
+//            img_cast.loadImage(mData[position])
+            Glide.with(this.img_cast).load(mData[position].imgLink).into(this.img_cast)
+            name_cast.text = mData[position].name
         }
     }
-
     override fun getItemCount() = mData.size
+
+    fun updateCast(newCasts: List<Cast>){
+        val diffResult = DiffUtil.calculateDiff(DiffCallBack(mData, newCasts){it.name})
+        mData = newCasts
+        diffResult.dispatchUpdatesTo(this)
+    }
 }
--- app/src/main/java/vanson/dev/movieapp/utils/DiffCallBack.kt
+++ app/src/main/java/vanson/dev/movieapp/utils/DiffCallBack.kt
@@ -2,24 +2,18 @@ package vanson.dev.movieapp.utils
 
 import androidx.recyclerview.widget.DiffUtil
 
-class DiffCallBackCast<T>(
+class DiffCallBack<T>(
     private val oldItems: List<T>,
     private val newItems: List<T>,
     private val getIdItem: (T) -> Any
-)  : DiffUtil.Callback(){
-    override fun getOldListSize(): Int {
-        TODO("Not yet implemented")
-    }
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldItems.size
 
-    override fun getNewListSize(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getNewListSize(): Int = newItems.size
 
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        getIdItem(oldItems[oldItemPosition]) == getIdItem(newItems[newItemPosition])
 
-    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldItems[oldItemPosition] == newItems[newItemPosition]
 }

d21513ce08f6de30f28f5db0eed595332db96447
VanSon999 <nguyenvanson95999@gmail.com>
2021-02-24 15:26:15 +0700

    fix home_screen

---
+++ app/src/main/java/vanson/dev/movieapp/data/models/new_popular_top/Movies.kt
@@ -0,0 +1,16 @@
+package vanson.dev.movieapp.data.models.new_popular_top
+
+
+import com.google.gson.annotations.SerializedName
+import vanson.dev.movieapp.data.models.movie_details.Movie
+
+data class PopularMovies(
+    @SerializedName("page")
+    val page: Int = 0,
+    @SerializedName("results")
+    val results: List<Movie> = listOf(),
+    @SerializedName("total_pages")
+    val totalPages: Int = 0,
+    @SerializedName("total_results")
+    val totalResults: Int = 0
+)

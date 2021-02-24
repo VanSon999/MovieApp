5f93ba749e8c5542231ccd0e33f8f82f6843cfab
VanSon999 <nguyenvanson95999@gmail.com>
2021-02-24 19:47:29 +0700

    fix sliderAdapter to fit with Movie models

--- app/src/main/java/vanson/dev/movieapp/adapter/SliderPagerAdapter.kt
+++ app/src/main/java/vanson/dev/movieapp/adapter/SliderPagerAdapter.kt
@@ -1,43 +1,62 @@
 package vanson.dev.movieapp.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.slide_item.view.*
 import vanson.dev.movieapp.R
+import vanson.dev.movieapp.data.api.POSTER_BASE_URL
+import vanson.dev.movieapp.data.models.movie_details.Movie
 import vanson.dev.movieapp.models.Slide
+import vanson.dev.movieapp.utils.DiffCallBack
+import vanson.dev.movieapp.utils.MovieItemClickListener
+import vanson.dev.movieapp.utils.loadBackImage
 import vanson.dev.movieapp.view_model.player.MoviePlayerActivity
 
-class SliderPagerAdapter(private val mContext: Context, private val mList: List<Slide>) :
+class SliderPagerAdapter(private val listener: MovieItemClickListener) :
     RecyclerView.Adapter<SliderPagerAdapter.ViewHolder>() {
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    private var mData: List<Movie> = listOf()
+    inner class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
         init {
-            itemView.floatingActionButton.setOnClickListener {
-                val intent = Intent(mContext, MoviePlayerActivity::class.java)
-                mContext.startActivity(intent)
+            this.view.slide_image.setOnClickListener {
+                listener.onMovieClick(mData[adapterPosition], this.view.slide_image)
+            }
+            this.view.floatingActionButton.setOnClickListener {
+                listener.onPlayClick(mData[adapterPosition])
             }
         }
     }
 
-    override fun getItemCount(): Int = mList.size
+    override fun getItemCount(): Int = mData.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.slide_item, parent, false)
+        return ViewHolder(view)
 //        val inflater = mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 //        val view = inflater.inflate(R.layout.slide_item, parent, false)
 //        view.layoutParams = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT) //fix match_parent pages
-        return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(holder.itemView){
-            slide_image.setImageResource(mList[position].getImage())
-            slide_title.text = mList[position].getTitle()
+        with(holder.view){
+            slide_image.loadBackImage(POSTER_BASE_URL + mData[position].backdropPath)
+            slide_title.text = mData[position].title + "\nMore info..."
         }
     }
+
+    fun updateMovie(newMovies: List<Movie>){
+        val diffResult = DiffUtil.calculateDiff(DiffCallBack(mData, newMovies){it.id})
+        mData = newMovies
+        diffResult.dispatchUpdatesTo(this)
+    }
 }
+
 //class SliderPagerAdapter(private val mContext: Context, private val mList: List<Slide>) :
 //    PagerAdapter() {
 //    override fun getCount(): Int {

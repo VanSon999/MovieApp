07e5bca4e071872a156e2415218b1cb1ac4b2919
VanSon999 <nguyenvanson95999@gmail.com>
2021-03-02 22:03:28 +0700

    fresh project

--- app/src/main/java/vanson/dev/movieapp/HomeActivity.kt
+++ app/src/main/java/vanson/dev/movieapp/HomeActivity.kt
@@ -10,15 +10,5 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        //prepare a list of slides...
-        stSlides = ArrayList()
-        stSlides.add(Slide(R.drawable.slide2, "Wolverine 2013\nMore text here..."))
-        stSlides.add(Slide(R.drawable.slide1, "Captain American: Winter Solider\nMore text here..."))
-        stSlides.add(Slide(R.drawable.slide2, "Wolverine 2013\nMore text here..."))
-        stSlides.add(Slide(R.drawable.slide1, "Captain American: Winter Solider\nMore text here..."))
-
-        val mAdapter = SliderPagerAdapter(this, stSlides)
-        slide_pager.adapter = mAdapter
     }
 }

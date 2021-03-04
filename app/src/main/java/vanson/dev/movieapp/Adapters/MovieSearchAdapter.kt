8accdbdd54609e087640ad6c4199f7f3f045b6bd
VanSon999 <nguyenvanson95999@gmail.com>
2021-03-04 23:10:59 +0700

    create ui search_item and adapter for it

--- app/src/main/java/vanson/dev/movieapp/Adapters/MovieSearchAdapter.kt
+++ app/src/main/java/vanson/dev/movieapp/Adapters/MovieSearchAdapter.kt
@@ -1,4 +1,44 @@
 package vanson.dev.movieapp.Adapters
 
-class MovieSearchAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.search_item.view.*
+import vanson.dev.movieapp.BuildConfig.BASE_URL_IMAGE
+import vanson.dev.movieapp.Models.Movie
+import vanson.dev.movieapp.R
+
+class MovieSearchAdapter: RecyclerView.Adapter<MovieSearchAdapter.ViewHolder>() {
+
+    private var results = listOf<Movie>()
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        init {
+            val generator = RandomTransitionGenerator(1000, DecelerateInterpolator())
+            itemView.diagonal_image.setTransitionGenerator(generator)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.itemView){
+            diagonal_image.loadPoster(results[position].posterPath.toString())
+            poster_title.text = results[position].title
+        }
+    }
+
+    override fun getItemCount(): Int = results.size
+
+}
+
+fun ImageView.loadPoster(uri: String?){
+    Picasso.get().load(BASE_URL_IMAGE + uri).into(this)
 }
--- app/src/main/java/vanson/dev/movieapp/HomeActivity.kt
+++ app/src/main/java/vanson/dev/movieapp/HomeActivity.kt
@@ -1,14 +1,94 @@
 package vanson.dev.movieapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_home.*
+import org.angmarch.views.NiceSpinner
+import org.angmarch.views.OnSpinnerItemSelectedListener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import vanson.dev.movieapp.Client.RetrofitClient
+import vanson.dev.movieapp.Interfaces.RetrofitService
+import vanson.dev.movieapp.Models.MoviesResponse
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var stSlides: ArrayList<Slide>
-
+    private lateinit var mRetrofitService: RetrofitService
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //disable keyboard when start
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+
+        mRetrofitService = RetrofitClient.getClient().create(RetrofitService::class.java)
+        results_recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val category = arrayListOf<String>("By Movie Title", "By Person Name")
+        val position = source_spinner.selectedIndex
+
+        if(position == 0){
+            query_edit_text.setText("Enter any movie title...")
+        }else{
+            query_edit_text.setText("Enter any person name...")
+        }
+
+        source_spinner.setOnSpinnerItemSelectedListener(object : OnSpinnerItemSelectedListener{
+            override fun onItemSelected(
+                parent: NiceSpinner?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if(position == 0){
+                    query_edit_text.setText("Enter any movie title...")
+                }else{
+                    query_edit_text.setText("Enter any person name...")
+                }
+            }
+
+        })
+
+        search_button.setOnClickListener {
+            if(query_edit_text.text != null){
+                val text_ = query_edit_text.text.toString()
+                if(text_ == "" || text_ == " "){
+                    Toast.makeText(this, "Please enter any text...", Toast.LENGTH_SHORT)
+                }else{
+                    query_edit_text.setText("")
+                    val query = text_.replace(" ", "+")
+                    if(category.size > 0){
+                        val categoryName = category[source_spinner.selectedIndex]
+                        if(source_spinner.selectedIndex == 0){
+                            val movieResponseCall = mRetrofitService.getMoviesByName(BuildConfig.API_KEY, query)
+                            movieResponseCall.enqueue(object : Callback<MoviesResponse>{
+                                override fun onResponse(
+                                    call: Call<MoviesResponse>,
+                                    response: Response<MoviesResponse>
+                                ) {
+                                    val movieResponse = response.body()
+                                    if(movieResponse != null){
+
+                                    }
+                                }
+
+                                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
+                                    TODO("Not yet implemented")
+                                }
+
+                            })
+                        }else{
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }

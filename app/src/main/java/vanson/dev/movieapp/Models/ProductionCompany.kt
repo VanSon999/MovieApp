080df3c4a0aee1f529bbc9d8ebb2af3389ef606e
VanSon999 <nguyenvanson95999@gmail.com>
2021-04-04 17:35:28 +0700

    prepare ui for movie details

---
+++ app/src/main/java/vanson/dev/movieapp/Models/ProductionCompany.kt
@@ -0,0 +1,15 @@
+package vanson.dev.movieapp.Models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductionCompany(
+    @SerializedName("id")
+    val id: Int = 0,
+    @SerializedName("logo_path")
+    val logoPath: String = "",
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("origin_country")
+    val originCountry: String = ""
+)

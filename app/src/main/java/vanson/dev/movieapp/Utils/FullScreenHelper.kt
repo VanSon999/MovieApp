fe6f50e17864474240afbd5931c80b9af944ec74
VanSon999 <nguyenvanson95999@gmail.com>
2021-04-17 22:42:49 +0700

    util for fullscreen

--- app/src/main/java/vanson/dev/movieapp/Utils/FullScreenHelper.kt
+++ app/src/main/java/vanson/dev/movieapp/Utils/FullScreenHelper.kt
@@ -1,4 +1,36 @@
 package vanson.dev.movieapp.Utils
 
-class FullScreenHelper {
+import android.app.Activity
+import android.view.View
+
+@Suppress("UNCHECKED_CAST", "DEPRECATION")
+class FullScreenHelper(private val activity: Activity, vararg views: View) {
+    private val views: Array<View> = views as Array<View>
+    private fun showSystemUi(decorView: View) {
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+    private fun hideSystemUi(decorView: View) {
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    }
+
+    fun enterFullScreen() {
+        val view = activity.window.decorView
+        hideSystemUi(view)
+        for (v in views) {
+            v.visibility = View.GONE
+            v.invalidate()
+        }
+    }
+
+    fun exitFullScreen() {
+        val view = activity.window.decorView
+        showSystemUi(view)
+        for (v in views) {
+            v.visibility = View.VISIBLE
+            v.invalidate()
+        }
+    }
+
 }

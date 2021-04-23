0431249ced641c309fcab90f6336a7a4dfbb813d
VanSon999 <nguyenvanson95999@gmail.com>
2021-04-23 12:44:36 +0700

    nothing

--- app/src/main/java/vanson/dev/movieapp/Adapters/ProfileImageAdapter.kt
+++ app/src/main/java/vanson/dev/movieapp/Adapters/ProfileImageAdapter.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.profile_image_layout.view.*
 import vanson.dev.movieapp.Adapters.commons.ImagesViewHolder
-import vanson.dev.movieapp.ImageViewerActivity
 import vanson.dev.movieapp.Models.ProfileImage
 import vanson.dev.movieapp.R
 import vanson.dev.movieapp.ScreenSlidePagerImageActivity
@@ -26,7 +25,7 @@ class ProfileImageAdapter(val activity: Activity, data: List<ProfileImage>) : Re
         with(holder.itemView){
             image_profile.loadProfileImage(mData[position].filePath)
             image_profile.setOnClickListener {
-                val images = mData.map { it.filePath }
+                val images = mData.map { it -> it.filePath }
                 val intent = Intent(activity, ScreenSlidePagerImageActivity::class.java)
                 intent.putExtra("current_position", position)
                 intent.putExtra("images", images.toTypedArray())

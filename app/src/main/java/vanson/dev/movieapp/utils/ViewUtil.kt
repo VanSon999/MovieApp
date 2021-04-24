0f887ed4ee61202cfbbe088694147a01ef66e543
VanSon999 <nguyenvanson95999@gmail.com>
2021-04-24 18:37:25 +0700

    change something in load Image View

--- app/src/main/java/vanson/dev/movieapp/utils/ViewUtil.kt
+++ app/src/main/java/vanson/dev/movieapp/utils/ViewUtil.kt
@@ -4,11 +4,12 @@ import android.app.Activity
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
-import androidx.core.widget.ImageViewCompat
 import com.bumptech.glide.Glide
 import com.flaviofaria.kenburnsview.KenBurnsView
 import com.jsibbold.zoomage.ZoomageView
 import vanson.dev.movieapp.R
+import vanson.dev.movieapp.data.api.BACK_BASE_URL
+import vanson.dev.movieapp.data.api.POSTER_BASE_URL
 
 private fun View.ifNotDestroyed(block: () -> Unit) {
     if (!(context as Activity).isDestroyed) {
@@ -17,37 +18,43 @@ private fun View.ifNotDestroyed(block: () -> Unit) {
 }
 
 fun ImageView.loadPhotoImage(url: String) = ifNotDestroyed {
-    Glide.with(this).load(url).placeholder(
+    Glide.with(this).load("$POSTER_BASE_URL$url").placeholder(
         R.drawable.default_avatar
     ).into(this)
 }
 
 fun ImageView.loadPosterImage(url: String) = ifNotDestroyed {
-    Glide.with(this).load(url).placeholder(
+    Glide.with(this).load("$POSTER_BASE_URL$url").placeholder(
         R.drawable.no_image
     ).into(this)
 }
 
 fun ImageView.loadBackImage(url: String) = ifNotDestroyed {
-    Glide.with(this).load(url).placeholder(
+    Glide.with(this).load("$BACK_BASE_URL$url").placeholder(
         R.drawable.sunset
     ).into(this)
 }
 
-fun KenBurnsView.loadPersonImage(url: String?) = ifNotDestroyed {
-    Glide.with(this).load("https://image.tmdb.org/t/p/w500$url").placeholder(
+fun KenBurnsView.loadPersonImage(url: String) = ifNotDestroyed {
+    Glide.with(this).load("$BACK_BASE_URL$url").placeholder(
         R.drawable.default_avatar
     ).into(this)
 }
 
-fun ZoomageView.loadImage(url: String?) = ifNotDestroyed {
-    Glide.with(this).load("https://image.tmdb.org/t/p/w500$url").placeholder(
+fun KenBurnsView.loadBackImage(url: String) = ifNotDestroyed {
+    Glide.with(this).load("$BACK_BASE_URL$url").placeholder(
+        R.drawable.no_image
+    ).into(this)
+}
+
+fun ZoomageView.loadImage(url: String) = ifNotDestroyed {
+    Glide.with(this).load("$BACK_BASE_URL$url").placeholder(
         R.drawable.default_avatar
     ).into(this)
 }
 
-fun AppCompatImageView.loadProfileImage(url: String?) = ifNotDestroyed{
-    Glide.with(this).load("https://image.tmdb.org/t/p/w500$url").placeholder(
+fun AppCompatImageView.loadProfileImage(url: String) = ifNotDestroyed {
+    Glide.with(this).load("$POSTER_BASE_URL$url").placeholder(
         R.drawable.default_avatar
     ).into(this)
 }
